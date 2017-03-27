087f1ba20cdf5d4ea0d5b2b1e730944f665dbbc6
ilyyya <ilyyy@ilYYYa-PC-Mini>
2017-03-27 17:56:49 +0300

    creating example

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,9 +1,71 @@
 
-public class Main {
+public class Main
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
 
 	}
-
 }
+
+class A
+{
+	byte b = 127;
+	short b2 = 32500;
+	int x;
+	int y = 2100000000;
+	long huge = 9000000000000000000L;
+	double hp = 20.0D;
+	float mp = 100.0F;
+	boolean fall = false;
+	boolean create = 40 < 5;
+	String str = "str";
+	B classB = new B();
+	void func()
+	{
+		//some code
+	}
+}
+
+class B
+{
+	int x = 1;
+	public int y = 2;
+	private int z = 3;
+	static int t = 4;
+	
+	B()
+	{
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
